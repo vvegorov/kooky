d12ad32822b4107306874ee6ed9a99a97a32caf5
vvegorov <egorovvvsls@gmail.com>
2017-12-03 20:50:09 +0300

    DEF-24

--- app/models/cryptid.js
+++ app/models/cryptid.js
@@ -4,6 +4,7 @@ export default DS.Model.extend({
   name: DS.attr('string'),
   cryptidType: DS.attr('string'),
   profileImg: DS.attr('string'),
+  // fileImg: DS.attr('string'),
   sightings: DS.hasMany('sighting'),
   cryptidName: Ember.computed('name', 'cryptidType', function() {
     return this.get('name') + ' ' + this.get('cryptidType');
--- app/routes/cryptids.js
+++ app/routes/cryptids.js
@@ -1,6 +1,12 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  // beforeModel() {
+  //   this.store.createRecord('cryptid', {
+  //     "name": "Charlie",
+  //     "cryptidType": "unicorn"
+  //   });
+  // },
   model() {
     return this.store.findAll('cryptid');
   }
